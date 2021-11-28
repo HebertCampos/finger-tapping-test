a857cd176ac522e40c444d5110f35d8238021c7e
Hebert Campos <hebertcdev@gmail.com>
2021-11-28 16:40:49 -0300

    Identificando as mãos

--- fingerTappingTest.py
+++ fingerTappingTest.py
@@ -5,9 +5,17 @@ import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     ret, img = cap.read()
     
+    imgBGR = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgBGR)
+    print(results.multi_hand_landmarks)
+
 
     cv2.imshow('img', img)
     cv2.waitKey(1)
