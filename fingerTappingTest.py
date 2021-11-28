98855cb438c3148a2d43688c9543acf8d2254177
Hebert Campos <hebertcdev@gmail.com>
2021-11-28 17:27:46 -0300

    comando para encerrar aplicação

--- fingerTappingTest.py
+++ fingerTappingTest.py
@@ -55,6 +55,6 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0),3)
 
     cv2.imshow('img', img)
-    cv2.waitKey(1)
-    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     
