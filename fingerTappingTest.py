c91839df2222191945cdc32ec7ae14d9dd6a8d8d
Hebert Campos <hebertcdev@gmail.com>
2021-11-28 16:47:12 -0300

    Pegando localização dos pontos dos dedos

--- fingerTappingTest.py
+++ fingerTappingTest.py
@@ -18,6 +18,11 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx,cy)
+            
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
 
